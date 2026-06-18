07fafb1fa55618f0e21cebf479379f6c1237b2e7
samarthadhav <samarthadhav2006@gmail.com>
2026-06-18 18:28:50 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ var mysql=require('mysql2');
 var util=require('util');
 
 var conn=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"Root",
-    database:"dental"
+    host:"bst5y8zdqvkxuwulbnt6-mysql.services.clever-cloud.com",
+    user:"ulecqmgooilav826",
+    password:"xMGx0VbZaS4KMF0qEJ11",
+    database:"bst5y8zdqvkxuwulbnt6"
 })
 
 var exe=util.promisify(conn.query).bind(conn);
